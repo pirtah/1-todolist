65cdfd3b2ebb78cf0a61d067cfa1932b124dcda2
unknown <pirtah_discord@outlook.fr>
2022-05-02 14:14:09 +0200

    setup commit

---
+++ App.js
@@ -0,0 +1,36 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+ /*export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.textColor}>Hello World !</Text>
+      <Text style={{color:'green'}}>De nouveau</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  textColor: {
+    color:'red',
+  },
+}); */
+
+// rnfes
+const App = () => {
+  return (
+    <View>
+      <Text>App</Text>
+    </View>
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({})
